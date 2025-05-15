582d60854f7f96314a2827560ef63c81126851d8
“RodrigoAranda” <rodrigo.aranda.developer@gmail.com>
2025-05-15 10:34:14 -0300

    eliminacion de errores

--- src/components/ScrollLink/scrollLink.tsx
+++ src/components/ScrollLink/scrollLink.tsx
@@ -31,22 +31,3 @@ export const ScrollLink: FC<ScrollLinkProps> = ({ href, children }) => {
     </a>
   );
 };
-
-// "use client";
-
-// import { useRouter } from "next/navigation";
-
-// export default function ScrollLink({ href, children }) {
-//   const router = useRouter();
-
-//   const handleClick = (e) => {
-//     e.preventDefault();
-//     router.push(`/#${href}`);
-//     setTimeout(() => {
-//       const element = document.getElementById(href);
-//       if (element) element.scrollIntoView({ behavior: "smooth" });
-//     }, 100); // Pequeño delay para asegurar la navegación
-//   };
-
-//   return <a href={`/#${href}`} onClick={handleClick}>{children}</a>;
-// }
